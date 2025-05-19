8922b6488911a3b0df9f3e10352681e0e04ab79c
Lupastean Iulian - Gabriel <ilupastean@yahoo.com>
2025-05-19 15:29:25 +0300

    Update main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "MapMyJourneyRG"
+    storage_account_name = "mapmyjourneytfstate"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
