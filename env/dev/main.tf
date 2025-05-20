c1a3ac99499074e828aaab390812bd037b26b0d0
Lupastean Iulian - Gabriel <ilupastean@yahoo.com>
2025-05-20 09:38:32 +0300

    Remove server version

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -58,10 +58,8 @@ resource "azurerm_mssql_server" "main" {
   name                         = var.sql_server_name
   resource_group_name          = azurerm_resource_group.main.name
   location                     = azurerm_resource_group.main.location
-  version                      = "12.0"
   administrator_login          = var.sql_admin_username
   administrator_login_password = var.sql_admin_password
-  minimum_tls_version          = "1.2"
 }
 
 resource "azurerm_mssql_database" "main" {
