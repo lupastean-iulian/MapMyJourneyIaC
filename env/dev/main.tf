e07a94818c2e711b3a559564ddd863126f44cb1b
Lupastean Iulian - Gabriel <ilupastean@yahoo.com>
2025-05-20 09:12:59 +0300

    Move always_on in site_config block

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -45,8 +45,8 @@ resource "azurerm_linux_web_app" "backend" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   service_plan_id     = azurerm_service_plan.main.id
-  always_on          = false
   site_config {
+    always_on          = false
     websockets_enabled = true
     application_stack {
       dotnet_version = "8.0"
